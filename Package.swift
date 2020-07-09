91fb8bf983d10d0a45754927a50ee629520ef432
August Saint Freytag <night@apricummedia.com>
2020-07-09 23:12:23 +0200

    Add package file with Swift Argument Parser dependency, add lock file

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "cockpit-sync",
+	platforms: [
+		// .linux,
+		.macOS(.v10_15)
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		.package(url: "https://github.com/apple/swift-argument-parser", from: "0.2.0")
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
+		.target(
+			name: "cockpit-sync",
+			dependencies: [
+				.product(name: "ArgumentParser", package: "swift-argument-parser")
+			]
+		)
+	]
+)
