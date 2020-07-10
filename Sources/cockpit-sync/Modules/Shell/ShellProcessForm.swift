d1327ed6a4a40c7da9794c09dcf324b036541e34
August Saint Freytag <night@apricummedia.com>
2020-07-10 14:44:43 +0200

    Introduce dedicated shell process form protocol for command execution

---
+++ Sources/cockpit-sync/Modules/Shell/ShellProcessForm.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+protocol ShellProcessForm {}
+
+extension ShellProcessForm {
+	
+	// MARK: Process Form
+
+	@discardableResult func execute(_ command: String) -> ShellStandardStreams? {
+		let process = Process()
+
+		process.executableURL = URL(fileURLWithPath: "/bin/bash")
+		process.arguments = ["-c", command]
+
+		let standardOutput = Pipe()
+		process.standardOutput = standardOutput
+
+		let standardError = Pipe()
+		process.standardError = standardError
+
+		do {
+			try process.run()
+		} catch {
+			assertionFailure("Could not execute wrapped command '\(command)'. \(error.localizedDescription)")
+			return nil
+		}
+
+		return ShellStandardStreams(standardOutput, standardError)
+	}
+	
+}
