25f934b0084388ac1da266431a704dc706e6a10f
August Saint Freytag <night@apricummedia.com>
2020-07-12 10:55:31 +0200

    Remove unneeded platform section from command package file

--- Package.swift
+++ Package.swift
@@ -5,10 +5,6 @@ import PackageDescription
 
 let package = Package(
 	name: "cockpit-sync",
-	platforms: [
-		// .linux,
-		.macOS(.v10_15)
-	],
 	dependencies: [
 		// Dependencies declare other packages that this package depends on.
 		.package(url: "https://github.com/apple/swift-argument-parser", from: "0.2.0")
