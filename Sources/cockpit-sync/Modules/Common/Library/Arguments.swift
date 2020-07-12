530e0cb8bb5c7063acaea6d50a8d6fb77f89011d
August Saint Freytag <night@apricummedia.com>
2020-07-12 10:47:58 +0200

    Establish dedicated custom string conversion for mode argument
    Allow separate description text for "everything" argument case.

--- Sources/cockpit-sync/Modules/Common/Library/Arguments.swift
+++ Sources/cockpit-sync/Modules/Common/Library/Arguments.swift
@@ -1,13 +1,34 @@
 import ArgumentParser
 
 enum Scope: String, Hashable, CaseIterable, ExpressibleByArgument {
+	
 	case everything
 	case structure
 	case records
+	
+}
+
+extension Scope: CustomStringConvertible {
+	
+	var defaultValueDescription: String {
+		return self.rawValue
+	}
+	
+	var description: String {
+		switch self {
+		case .everything:
+			return "all structure and records"
+		default:
+			return self.rawValue
+		}
+	}
+	
 }
 
 enum Mode: String, Hashable, CaseIterable, ExpressibleByArgument {
+	
 	case clear
 	case save
 	case restore
+	
 }
