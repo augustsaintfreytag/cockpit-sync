f80b5cc007e8e891e3cde86e42c69d07760a74b1
August Saint Freytag <night@apricummedia.com>
2020-07-15 11:07:38 +0200

    Extend set-up functionality for cockpit archive and in-volume dirs.
    Allow presence check and recursive creation for all defined scopes.

--- Sources/cockpit-sync/Modules/Operations/CockpitDirectoryPreparation.swift
+++ Sources/cockpit-sync/Modules/Operations/CockpitDirectoryPreparation.swift
@@ -1,20 +1,75 @@
-protocol CockpitDirectoryPreparation: CockpitPathForm, ShellExecutionForm {}
+protocol CockpitDirectoryPreparation: ShellAssertedExecutionForm, CockpitDockerForm {}
 
 extension CockpitDirectoryPreparation {
-	
-	func archiveDirectoriesExist(for scope: Scope, archivePath: Path) -> Bool {
+
+	/// Checks if the archive and its subdirectories exist to run an operation in the given scope.
+	func archiveDirectoriesExist(for scope: Scope, archivePath: Path) throws -> Bool {
 		let archiveDirectoryPaths = directoryHierarchyPathComponents(for: scope).map { pathComponent in
-			return "\(archivePath)/\(pathComponent)"
+			return "'\(archivePath)/\(pathComponent)'"
 		}
-		
-		for path in archiveDirectoryPaths {
-			guard let result = execute("stat '\(path)'"), result.hasError == false else {
-				print("Path '\(path)' is missing.")
-				return false
-			}
+
+		guard let result = execute("stat \(archiveDirectoryPaths.joined(separator: " "))"), !result.hasError else {
+			throw ExecutionError(
+				errorDescription: "Could not stat archive directories for provided paths in scope '\(scope)'. Checked paths: \(archiveDirectoryPaths.joined(separator: ", "))."
+			)
 		}
-		
+
+		guard !result.hasError else {
+			return false
+		}
+
 		return true
+	}
+
+	/// Checks if the cockpit root and its subdirectories exist in-volume.
+	func cockpitDirectoriesExist(for scope: Scope, volumeName: String) throws -> Bool {
+		let containerizedDirectoryNames = containerizedCockpitDirectoryNames(for: scope)
+		let volumeMountArgument = dockerVolumeMountArgument(volumeName: volumeName)
+		let command = containerizedCommand("stat \(containerizedDirectoryNames.joined(separator: " "))", mounting: [volumeMountArgument])
+
+		guard let result = execute(command) else {
+			throw ExecutionError(
+				errorDescription: "Could not stat in-volume Cockpit directories for provided paths in scope '\(scope)'. Checked paths: \(containerizedDirectoryNames.joined(separator: ", "))."
+			)
+		}
+
+		guard !result.hasError else {
+			return false
+		}
+
+		return true
+	}
+
+	/// Creates all directories in the target Docker volume for the given scope.
+	///
+	/// Uses `mkdir` command to create directories as needed and gracefully skip the operation
+	/// if any destination directories already exist (returning 0 for such runs).
+	func setUpCockpitDirectories(for scope: Scope, volumeName: String) throws {
+		let containerizedDirectoryNames = containerizedCockpitDirectoryNames(for: scope)
+		let volumeMountArgument = dockerVolumeMountArgument(volumeName: volumeName)
+		let containerizedDirectorySetUpCommand = "mkdir -p \(containerizedDirectoryNames.joined(separator: " "))"
+		let command = containerizedCommand(containerizedDirectorySetUpCommand, mounting: [volumeMountArgument])
+
+		try executeAndAssert(command)
+	}
+
+	// MARK: Command Argument Form
+
+	private func containerizedCockpitDirectoryNames(for scope: Scope) -> [String] {
+		return cockpitDirectoryNames(for: scope).map { directoryName in
+			return "'\(containerizedCockpitPath)/\(directoryName)'"
+		}
+	}
+
+	private func cockpitDirectoryNames(for scope: Scope) -> [String] {
+		switch scope {
+		case .structure:
+			return ["cache", "collections", "singleton", "tmp"]
+		case .records:
+			return ["data", "thumbs", "uploads"]
+		case .everything:
+			return reduce(allCasesIn: cockpitDirectoryNames, excluding: .everything)
+		}
 	}
 	
 }
