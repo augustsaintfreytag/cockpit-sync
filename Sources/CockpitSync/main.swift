47d42cbcee3f96fbd18e755735f06cd107add258
August Saint Freytag <night@apricummedia.com>
2020-12-12 12:56:08 +0100

    Add manifest file, add version string printout to command structure

--- Sources/CockpitSync/main.swift
+++ Sources/CockpitSync/main.swift
@@ -14,7 +14,8 @@ struct CockpitSync: ParsableCommand, VolumePreparer, InVolumeDirectoryPreparer, 
 			"Assumes that Cockpit runs in a Docker container with its own dedicated volume mounted",
 			"at `/var/www/html/storage` inside the container environment. If the targeted Cockpit instance",
 			"runs openly outside of containerization, no special tools are required to save and restore its data."
-		)
+		),
+		version: "\(Manifest.name), Version \(Manifest.versionDescription)"
 	)
 	
 	// MARK: Properties
