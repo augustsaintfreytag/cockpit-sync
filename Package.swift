69d3bd23fc40e0e355bf68c923152633d940e72b
August Saint Freytag <night@apricummedia.com>
2020-12-12 12:55:04 +0100

    Rename remaining mentions of cockpit sync, use uniform case

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-	name: "cockpit-sync",
+	name: "CockpitSync",
 	platforms: [
 		// .linux — Support implicitly provided by default.
 		.macOS(.v10_15)
@@ -17,7 +17,7 @@ let package = Package(
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
 		.target(
-			name: "cockpit-sync",
+			name: "CockpitSync",
 			dependencies: [
 				.product(name: "ArgumentParser", package: "swift-argument-parser")
 			]
