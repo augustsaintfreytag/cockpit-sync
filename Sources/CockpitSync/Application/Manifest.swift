d185cc8491e2e41f57f8c9748cdb369bdfa7db23
August Saint Freytag <night@apricummedia.com>
2020-12-19 15:29:25 +0100

    Bump utility version to 1.2.0 in manifest

--- Sources/CockpitSync/Application/Manifest.swift
+++ Sources/CockpitSync/Application/Manifest.swift
@@ -2,7 +2,7 @@
 enum Manifest {
 	
 	static let name = "Cockpit Sync"
-	static let releaseVersion = "1.1.0"
+	static let releaseVersion = "1.2.0"
 	static let debugVersion =  "\(releaseVersion) Debug Preview"
 	
 	static var versionDescription: String {
